ccb6d6847761fa660c689b5eb920689065ae8d9b
Roman Ceresnak <romanceresnak@windowslive.com>
2019-06-20 08:19:21 +0200

    first commit

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type problem struct{
@@ -14,6 +15,7 @@ type problem struct{
 
 func main(){
 	csvFile := flag.String("csv","problem.csv","Parsong")
+	timeLimit := flag.Int("limit",2,"the time limit")
 
 	//set all the values
 	flag.Parse()
@@ -38,14 +40,30 @@ func main(){
 	arrayOfObject := parseLines(lines)
 	// current state[{5+5 10} {7+3 10} {1+1 2} {8+3 11} {1+2 3} {8+6 14} {3+1 4} {1+4 5} {5+1 6} {2+3 5} {3+3 6} {2+4 6} {5+2 7}]
 
-
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 	correctAnswer := 0
+
+problemloop:
 	for _,line:= range arrayOfObject {
 		fmt.Println("Question is", line.question)
-		var answer string
-		fmt.Scanf("%s\n",&answer)
-		if answer == line.answer{
-			correctAnswer++
+		answerCh := make(chan string)
+		go func(){
+			var answer string
+			fmt.Scanf("%s\n",&answer)
+			answerCh <- answer
+		}()
+
+
+	select {
+		case <-timer.C:
+			fmt.Println()
+			break problemloop
+
+			//the value from channel insert to answer variable
+		case answer := <-answerCh:
+			if answer == line.answer {
+				correctAnswer++
+			}
 		}
 	}
 	fmt.Println("You scored %d out of %d.\n",correctAnswer, len(arrayOfObject))
